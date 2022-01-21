86f1737322056872bc4d1c6692c951b5022d3ddf
KyzonP <92440861+KyzonP@users.noreply.github.com>
2022-01-21 21:44:58 +0000

    Added draggable 'card/biscuit', started scheuld work

---
+++ Control.gd
@@ -0,0 +1,20 @@
+extends Node
+
+export (int) var morality = 50
+export (int) var hunger = 50
+export (int) var social = 50
+export (int) var energy = 50
+export (int) var money = 50
+export (int) var happiness = 50
+export (bool) var infected = false 
+
+export (bool) var dragging = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
